3b283d2a59c625e88ad0c33e93d0d2105af98db4
taraszh <taras.rmrf@gmail.com>
2020-07-13 06:29:44 -0700

    db dump

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -1,187 +1,173 @@
--- phpMyAdmin SQL Dump
--- version 4.9.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
 --
--- Host: eu-west-1.rds.amazonaws.com:3306
--- Generation Time: Mar 30, 2020 at 12:18 AM
--- Server version: 8.0.16
--- PHP Version: 7.0.33-0ubuntu0.16.04.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: ci_test
+-- ------------------------------------------------------
+-- Server version	8.0.20-0ubuntu0.20.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test_task`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `boosterpack`
 --
 
+DROP TABLE IF EXISTS `boosterpack`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `boosterpack` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `price` decimal(10,2) NOT NULL DEFAULT '0.00',
   `bank` decimal(10,2) NOT NULL DEFAULT '0.00',
   `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `boosterpack`
 --
 
-INSERT INTO `boosterpack` (`id`, `price`, `bank`, `time_created`) VALUES
-(1, '5.00', '0.00', '2020-03-30 00:17:28'),
-(2, '20.00', '0.00', '2020-03-30 00:17:28'),
-(3, '50.00', '0.00', '2020-03-30 00:17:28');
-
--- --------------------------------------------------------
+LOCK TABLES `boosterpack` WRITE;
+/*!40000 ALTER TABLE `boosterpack` DISABLE KEYS */;
+INSERT INTO `boosterpack` VALUES (1,5.00,2.00,'2020-03-30 00:17:28','2020-07-13 13:21:11'),(2,20.00,2.00,'2020-03-30 00:17:28','2020-07-13 13:21:08'),(3,50.00,20.00,'2020-03-30 00:17:28','2020-07-13 13:21:00');
+/*!40000 ALTER TABLE `boosterpack` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `comment`
 --
 
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comment` (
-  `id` int(11) NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `assign_id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `assign_id` int unsigned NOT NULL,
   `text` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `likes` int DEFAULT NULL,
   `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`id`, `user_id`, `assign_id`, `text`, `time_created`) VALUES
-(1, 1, 1, 'Ну чо ассигн проверим', '2020-03-27 21:39:44'),
-(2, 1, 1, 'Второй коммент', '2020-03-27 21:39:55'),
-(3, 2, 1, 'Второй коммент от второго человека', '2020-03-27 21:40:22');
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,1,1,'Ну чо ассигн проверим',8,'2020-03-27 21:39:44','2020-07-13 09:40:53'),(2,1,1,'Второй коммент',NULL,'2020-03-27 21:39:55','2020-07-11 17:09:17'),(3,2,1,'Второй коммент от второго человека',NULL,'2020-03-27 21:40:22','2020-07-11 17:09:17'),(4,1,1,'789',5,'2020-07-12 14:06:52','2020-07-12 16:27:58'),(5,1,1,'123',0,'2020-07-12 14:16:34','2020-07-12 16:27:58'),(6,1,1,'65756',1,'2020-07-12 14:41:52','2020-07-13 07:48:33'),(7,1,1,'1111',6,'2020-07-12 14:53:08','2020-07-13 07:48:36'),(8,1,1,'22222',NULL,'2020-07-12 14:53:35','2020-07-12 14:53:35'),(9,1,1,'3333333',NULL,'2020-07-12 14:53:51','2020-07-12 14:53:51'),(10,1,1,'444444',NULL,'2020-07-12 14:54:38','2020-07-12 14:54:38'),(11,1,2,'232',8,'2020-07-13 09:58:17','2020-07-13 13:22:44'),(12,1,2,'rrgerg',NULL,'2020-07-13 13:22:39','2020-07-13 13:22:39');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `migrations` (
+  `version` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (2);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `post`
 --
 
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
   `text` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `likes` int DEFAULT NULL,
   `img` varchar(1024) DEFAULT NULL,
   `time_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`id`, `user_id`, `text`, `img`, `time_created`) VALUES
-(1, 1, 'Тестовый постик 1', '/images/posts/1.png', '2018-08-30 13:31:14'),
-(2, 1, 'Печальный пост', '/images/posts/2.png', '2018-10-11 01:33:27');
-
--- --------------------------------------------------------
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES (1,1,'Тестовый постик 1',15,'/images/posts/1.png','2018-08-30 13:31:14','2020-07-13 09:40:52'),(2,1,'Печальный пост',1,'/images/posts/2.png','2018-10-11 01:33:27','2020-07-13 09:58:14');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(60) DEFAULT NULL,
   `password` varchar(32) DEFAULT NULL,
   `personaname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
   `avatarfull` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
-  `rights` tinyint(4) NOT NULL DEFAULT '0',
+  `rights` tinyint NOT NULL DEFAULT '0',
   `wallet_balance` decimal(10,2) NOT NULL DEFAULT '0.00',
   `wallet_total_refilled` decimal(10,2) NOT NULL DEFAULT '0.00',
   `wallet_total_withdrawn` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `likes_total` int DEFAULT NULL,
   `time_created` datetime NOT NULL,
-  `time_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `time_created` (`time_created`),
+  KEY `time_updated` (`time_updated`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `personaname`, `avatarfull`, `rights`, `wallet_balance`, `wallet_total_refilled`, `wallet_total_withdrawn`, `time_created`) VALUES
-(1, 'admin@niceadminmail.pl', NULL, 'AdminProGod', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/96/967871835afdb29f131325125d4395d55386c07a_full.jpg', 0, '0.00', '0.00', '0.00', '2019-07-26 01:53:54'),
-(2, 'simpleuser@niceadminmail.pl', NULL, 'simpleuser', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/86/86a0c845038332896455a566a1f805660a13609b_full.jpg', 0, '0.00', '0.00', '0.00', '2019-07-26 01:53:54');
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin@niceadminmail.pl','123','AdminProGod','https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/96/967871835afdb29f131325125d4395d55386c07a_full.jpg',0,3.00,108.00,105.00,1077,'2019-07-26 01:53:54','2020-07-13 13:22:44'),(2,'simpleuser@niceadminmail.pl',NULL,'simpleuser','https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/86/86a0c845038332896455a566a1f805660a13609b_full.jpg',0,0.00,0.00,0.00,NULL,'2019-07-26 01:53:54','2020-07-11 17:09:17');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `boosterpack`
---
-ALTER TABLE `boosterpack`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `time_created` (`time_created`),
-  ADD KEY `time_updated` (`time_updated`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `boosterpack`
---
-ALTER TABLE `boosterpack`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `comment`
---
-ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-13  6:28:01
